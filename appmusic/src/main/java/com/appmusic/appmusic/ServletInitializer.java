c7a828c87fb4cf4dddff72180875ee547d061ef0
Trịnh Long <longtrinhvan97@gmail.com>
2021-05-15 14:26:28 +0700

    setup api

---
+++ appmusic/src/main/java/com/appmusic/appmusic/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.appmusic.appmusic;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppmusicApplication.class);
+	}
+
+}
